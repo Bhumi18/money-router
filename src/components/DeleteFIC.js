429c200a507d4e997c5884c56e6a35c7e082c135
Sadariya Bhumi <bhumisadariya18@gmail.com>
2023-01-18 15:02:41 +0530

    integration

--- src/components/DeleteFIC.js
+++ src/components/DeleteFIC.js
@@ -2,6 +2,10 @@ import React, { useEffect, useState } from "react";
 import { FormControl, MenuItem, Select } from "@mui/material";
 import { ConnectButton } from "@rainbow-me/rainbowkit";
 import { useAccount } from "wagmi";
+import { ethers } from "ethers";
+import { Framework } from "@superfluid-finance/sdk-core";
+import MoneyRouterABI from "../artifacts/MoneyRouter.json"
+const moneyRouterAddress = "0x3b05Df0482457891d48406736516679EE7B3a88c"
 
 function DeleteFIC() {
   const [indexValue, setIndexValue] = useState("");
@@ -13,6 +17,42 @@ function DeleteFIC() {
 
   const [loadingAnim, setLoadingAnim] = useState(false);
   const [btnContent, setBtnContent] = useState("Delete Flow");
+
+  const deleteFlowIntoContract = async () => {
+    try {
+      const { ethereum } = window;
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+
+        const sf = await Framework.create({
+          chainId: 5,
+          provider: provider,
+        });
+
+        const daix = await sf.loadSuperToken("fDAIx")
+        const moneyRouter = new ethers.Contract(
+          moneyRouterAddress,
+          MoneyRouterABI,
+          signer
+        )
+
+        //call money router create flow into contract method from signers[0]
+        //this flow rate is ~2000 tokens/month
+        await moneyRouter
+          .connect(signer)
+          .deleteFlowIntoContract(daix.address)
+          .then(function (tx) {
+            console.log(`
+            Congrats! You just successfully delete a flow that was being sent into the money router contract. 
+            Tx Hash: ${tx.hash}
+        `)
+          })
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
   return (
     <div className="db-sub">
       <h1>Delete Flow Into Contract</h1>
@@ -33,9 +73,9 @@ function DeleteFIC() {
               fontSize: "1rem",
               padding: "0px 5px",
               ".css-11u53oe-MuiSelect-select-MuiInputBase-input-MuiOutlinedInput-input.MuiSelect-select":
-                {
-                  minHeight: "auto",
-                },
+              {
+                minHeight: "auto",
+              },
               ".MuiOutlinedInput-notchedOutline": {
                 borderColor: "rgb(224, 224, 224)",
                 boxShadow: "rgba(204, 204, 204, 0.25) 0px 0px 6px 3px",
@@ -75,7 +115,7 @@ function DeleteFIC() {
 
         <div className="subscriber-add-btn">
           {isConnected ? (
-            <button className="action-btn">
+            <button className="action-btn" onClick={() => deleteFlowIntoContract()}>
               {loadingAnim ? <span className="loader"></span> : btnContent}
             </button>
           ) : (
